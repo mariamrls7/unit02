7460c6bea80b83a1e3474cbba8f0eb42ede21e62
mariamrls7 <122957790+mariamrls7@users.noreply.github.com>
2023-02-09 01:49:13 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,8 @@
+fun main() {
+  var name = "Maria" // String
+  val age = "33" // int
+  val annualPay = "150000" // A Double
+  
+  println("My name is $name, my age is $age. I hope to earn $annualPay per year.")
+  }
+  
